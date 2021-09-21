c9c6f53db7b42ae61d92d58c1f9c0485f24dc670
manak1 <mana23happy@hotmail.co.jp>
2021-09-22 02:28:47 +0900

    chore: eslintのルール調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   rules: {
-    'no-unused-vars': ['error']
+    'no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error']
+      }
+    ]
   }
 }
