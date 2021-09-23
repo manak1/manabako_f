e3ae2801eb58d1138acc97170b408aac52174cb3
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 06:24:28 +0900

    fix: 画像ローカルで読み込むように修正

--- src/components/pages/home/Profile/index.tsx
+++ src/components/pages/home/Profile/index.tsx
@@ -6,7 +6,7 @@ const Profile:React.VFC = () => {
   return (
   <section className={styles.profile}>
       <div className={styles.box}>
-        <img className={styles.logo} src='https://pbs.twimg.com/profile_images/1440932289899159553/Q2sQETvo_400x400.png' alt="manakiアイコン" />
+        <img className={styles.logo} src='/manaki.png' alt="manakiアイコン" />
         <div className={styles.bio}>
           <h2 className={styles.name}>まなき</h2>
           <a className={styles.link} href="https://twitter.com/mikeanakida">@mikeanakida</a>
