aafa9864a694ce0f5b1a65ba9da219295ee01846
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 02:17:09 +0900

    feat: 質問カードコンポーネントの作成

---
+++ src/components/pages/home/QuestionCard/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from "./styles.module.scss"
+
+type Props = {
+  question: string
+}
+
+const QuestionCard: React.VFC<Props> = ({ question }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.box}>
+        <p className={styles.question}>
+         { question }
+       </p>
+     </div>
+     <img src="/ogp_small.png" alt="ogp" className={styles.bg} />
+  </div>
+  );
+};
+
+export default QuestionCard;
