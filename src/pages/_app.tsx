911b3f00a2d8e09b4ad90bdc9008f5591b13e72f
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 02:13:15 +0900

    feat: font-familyの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.scss'
+import React from 'react';
+import { AppProps } from "next/app"
+import Head from 'next/head';
+import 'modern-css-reset'
+import './_app.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App: React.FC<AppProps> = ({Component, pageProps}) => {
+  return (
+    <>
+      <Head>   
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  );
+};
 
-export default MyApp
+export default App;
