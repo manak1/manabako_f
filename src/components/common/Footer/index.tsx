25ae901578323ab969437348e4ff5c492ac89722
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 02:16:03 +0900

    feat: フッターコンポーネントの作成

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./styles.module.scss"
+
+
+const Footer:React.VFC = () => {
+  return (
+    <footer className={styles.footer}>
+      <small className={styles.copy}>&copy; hungrypenguin</small>
+    </footer>
+  );
+};
+
+export default Footer;
