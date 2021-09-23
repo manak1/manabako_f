7e6877d273237d3a04a40e22c612d0b626215e32
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 06:27:06 +0900

    feat: 質問入力簡易バリデーションの実装

--- src/components/pages/home/Question/index.tsx
+++ src/components/pages/home/Question/index.tsx
@@ -1,11 +1,12 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import styles from "./styles.module.scss"
 
 import SimpleButton from "~/components/button/SimpleButton"
 import InputTextArea from "~/components/input/InputTextArea"
 
+
 type Props = {
-  onClickSend: ()=> void
+  onClickSend: (question: string)=> void
 }
 
 const Question: React.VFC<Props> = ({onClickSend}) => {
@@ -15,12 +16,21 @@ const Question: React.VFC<Props> = ({onClickSend}) => {
     return !!(question.length === 0)
   },[question])
 
+  const isInValid = useMemo(()=> {
+    return !(question.length >=20 && question.length <=300)
+  },[question])
+
+  const onClick= useCallback(()=> {
+    onClickSend(question)
+    setQuestion('')
+  },[onClickSend, question])
+
   return (
     <div className={styles.question}>
       <h2 className={styles.title}>まなきになんでも質問しよう!</h2>
       <form className={styles.form}>
         <InputTextArea value={question} onChange={setQuestion} />
-        <SimpleButton label="質問を送信する" disabled={isDisabled} onClick={onClickSend} />
+        <SimpleButton label="質問を送信する" disabled={isDisabled || isInValid} onClick={onClick} />
       </form>
     </div>
   );
