ae5227eb12cff38794d26ba88b55522444ae32ef
manak1 <mana23happy@hotmail.co.jp>
2021-09-23 15:03:55 +0900

    feat: svg importできるように

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.resolve.alias['~'] = path.join(__dirname, 'src')
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
     return config
   },
   sassOptions: {
