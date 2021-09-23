e235537446669a547277712377b892c7494c9cba
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 02:15:18 +0900

    feat: ボタンコンポーネントの作成

---
+++ src/components/button/SimpleButton/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.module.scss'
+import clsx from "clsx"
+
+type Props = {
+  label: string,
+  onClick: ()=> void
+  disabled?: boolean
+}
+
+const SimpleButton: React.VFC<Props> = ({label, onClick, disabled}) => {
+  return (
+    <div className={styles.simpleButton}>
+      <button className={clsx(styles.button, disabled && styles.disabled)} onClick={onClick} disabled={disabled}>
+        { label }
+      </button>
+    </div>
+  );
+};
+
+export default SimpleButton;
