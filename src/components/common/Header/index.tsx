3932db27b67bc46794d61d64e6b344a29bd25c0d
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 06:20:25 +0900

    feat: ロゴからホームに飛べるように

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import styles from "./styles.module.scss"
 
 import Logo from "~/assets/images/icons/logo.svg"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.box}>
-        <Logo class={styles.logo}  alt="manabakoロゴ" />
-        <h1 className={styles.title}>manabako</h1>
-      </div>
+        <Link href="/" passHref>
+          <div className={styles.box}>
+            <Logo className={styles.logo}  alt="manabakoロゴ" />
+            <h1 className={styles.title}>manabako</h1>
+          </div>
+        </Link>
     </header>
   )
 }
