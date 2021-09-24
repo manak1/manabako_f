aee97c2bcf5536b02580f0ed9ff3f37ef5c7fced
manak1 <mana23happy@hotmail.co.jp>
2021-09-25 07:02:40 +0900

    feat: loaderの配置

--- src/components/pages/home/Answered/index.tsx
+++ src/components/pages/home/Answered/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 import QuestionCard from '~/components/pages/home/QuestionCard'
+import Loader from "react-loader-spinner";
 
 import { Question } from '~/@types/question'
 import Link from 'next/link'
@@ -14,9 +15,10 @@ const Answered: React.VFC<Props> = ({ questions }) => {
   return (
     <section className={styles.answered}>
       <h2 className={styles.title}>まなきが過去に答えた質問</h2>
-      <ul className={styles.list}>
-        {questions &&
-          questions.map((question) => {
+      {
+        questions ?
+        <ul className={styles.list}>
+          {questions.map((question) => {
             return (
               <li key={question.id} className={styles.item}>
                 <Link href={`/question/${question.id}`} passHref>
@@ -26,11 +28,16 @@ const Answered: React.VFC<Props> = ({ questions }) => {
                 </Link>
               </li>
             )
-          })}
+      })
+      }    
       </ul>
+      :
+      <div className={styles.loader}>
+      <Loader type='ThreeDots' color="rgb(29, 161, 242)" width={30} height={30} />
+      </div>
+      }
     </section>
   )
 }
 
 export default Answered
-//l_text:Sawarabi%20Gothic_40_bold:これはOGPテキストこれはこれはOGPテキストです！テこれはOGPテキストです！キストです！です！,co_rgb:333,w_1000,c_fit,y_-100,h_860
