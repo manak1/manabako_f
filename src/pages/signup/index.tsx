81200dfe93b08651acd1ec31f5650455a85d7189
manak1 <mana23happy@hotmail.co.jp>
2021-09-25 02:50:49 +0900

    style: 自動整形による整形

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-import Layout from "~/layout/HeaderFooterLayout"
+import React, { useState } from 'react'
+import Layout from '~/layout/HeaderFooterLayout'
 
-import { useSignup } from '~/hooks/api/auth';
+import { useSignup } from '~/hooks/api/auth'
 
 const Signin = () => {
-  const [password,setPassword] = useState('')
-  const [username,setUsername]= useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
 
-  const {fetch, progress} = useSignup()
+  const { fetch, progress } = useSignup()
 
-  const onSubmit = async()=> {
-    if(progress) return
-    await fetch(username,password).catch((error)=> {throw error})
+  const onSubmit = async () => {
+    if (progress) return
+    await fetch(username, password).catch((error) => {
+      throw error
+    })
   }
 
   return (
     <Layout>
       <div>
-        <label >username</label>
-        <input value={username} onChange={(event)=> setUsername(event.target.value)} type="password"/>
+        <label>username</label>
+        <input
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          type="password"
+        />
       </div>
       <div>
-        <label >password</label>
-        <input value={password} onChange={(event)=> setPassword(event.target.value)} type="username"/>
+        <label>password</label>
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="username"
+        />
       </div>
       <button type="button" onClick={onSubmit}>
         登録する
       </button>
     </Layout>
-  );
-};
+  )
+}
 
-export default Signin;
+export default Signin
