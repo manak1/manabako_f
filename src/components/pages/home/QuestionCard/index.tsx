2003320772307528e7949e2af4b970cd58bd3308
manak1 <mana23happy@hotmail.co.jp>
2021-09-24 19:35:19 +0900

    feat: 質問カードコンポーネントの作成

--- src/components/pages/home/QuestionCard/index.tsx
+++ src/components/pages/home/QuestionCard/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styles from './styles.module.scss'
+import clsx from 'clsx'
 
 type Props = {
   question: string
+  showFull?: boolean
 }
 
-const QuestionCard: React.VFC<Props> = ({ question }) => {
+const QuestionCard: React.VFC<Props> = ({ question, showFull }) => {
+  const isTextOver = useMemo(() => {
+    return question.length >= 160
+  }, [question])
+
+  const shortenQuestion = useMemo(() => {
+    return isTextOver && !showFull
+      ? `${question.substring(0, 157)}...`
+      : question
+  }, [question, isTextOver, showFull])
+
   return (
     <div className={styles.card}>
-      <div className={styles.box}>
-        <p className={styles.question}>{question}</p>
+      <div className={clsx(styles.box, showFull && isTextOver && styles.over)}>
+        <p className={styles.question}>{shortenQuestion}</p>
       </div>
-      <img src="/ogp.png" alt="ogp" className={styles.bg} />
+      {showFull ? (
+        isTextOver ? (
+          <img src="/ogp_large.png" alt="ogp" />
+        ) : (
+          <img src="/ogp_small.png" alt="ogp" />
+        )
+      ) : (
+        <img src="/ogp_small.png" alt="ogp" className={styles.bg} />
+      )}
     </div>
   )
 }
